3295f8106160a15c2708da9cedfba55cf4299263
miruna-iosub <mirunaelenaiii2403@gmail.com>
2023-04-09 21:13:51 +0300

    Added js function to Add a product file

--- scripts/addaproduct.js
+++ scripts/addaproduct.js
@@ -4,26 +4,33 @@ function addProduct() {
     var productionDate = document.getElementById("production-date").value;
     var productCategory = document.getElementById("category").value;
     var productSubcategory = document.getElementById("subcategory").value;
-    var questions = document.getElementById("question").value;
+    //var questions = document.getElementById("question").value;
     var newQuestion = document.getElementById("new-question").value;
     var desc = document.getElementById("description").value;
 
+    const question1Checked = document.querySelector('input[name="question1"]').checked;
+    const question2Checked = document.querySelector('input[name="question2"]').checked;
+    const question3Checked = document.querySelector('input[name="question3"]').checked;
+    const question4Checked = document.querySelector('input[name="question4"]').checked;
+
+
     if (productName == "" || productCategory == "" || 
-    productSubcategory == "" || questions == "" || 
+    productSubcategory == "" ||
     newQuestion == "" || desc == "") {
         alert("Please fill all the fields!");
         return false;
     }  
 
-    if (productName.length < 5) {
+    else if (productName.length < 5) {
         alert("The name of the product must contain at least 5 characters."); 
         return false;       
     }
-
-    if(productCategory != "service" || productCategory != "product" ||
-    productCategory != "person" || productCategory != "aristic artefact" ||
-    productCategory != "geographical place"){
-        alert("The category you entered is wrong. Please check the categories from the menu."); 
-        return false;       
-    }
+    
+    else if (!question1Checked && !question2Checked && !question3Checked && !question4Checked) {
+        alert('Please choose a question.');
+    } 
+    else {
+        window.location = "../pages/signupconfirmation.html";
+        return true;
+}
 }
