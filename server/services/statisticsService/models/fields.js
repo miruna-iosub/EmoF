644d224b4bdafdf6c12a417c98de61855a4737ec
elenaopait <opaitelena@gmail.com>
2023-06-18 21:02:57 +0300

    .

--- server/services/statisticsService/models/fields.js
+++ server/services/statisticsService/models/fields.js
@@ -4,8 +4,9 @@ class DefaultFields {
     personFields=['performance','communication','personality','attitude','behaviour','importance','socialImpact','interaction','style','views','interaction'];
     geoPlaceFields=['culture','weather','people','food','importance','history','pedestrianFriendly','worthVisiting','views','mood','expenses'];
     productFields=['utility','pricing','importance','performance','design','quality','safety','durability','rating','brand','marketing'];
-    serviceFields;
-    artisticArtefactFields;
+    serviceFields=['serviceFields','pricing','safety','usefulness','duration','quality','staff','importance','transparency','marketing','background-firm'];
+    artisticArtefactFields=['importance','message','worth','pricing','rating','author','vision','representation-of-culture','location','background','appearance']
+
     constructor() {
     }
 
